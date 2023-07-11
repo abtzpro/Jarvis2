1f3508174f39ba72a2aa228947bf180579c8c924
Adam R <121298839+abtzpro@users.noreply.github.com>
2023-07-11 02:09:54 -0500

    Update Jarvis.py
    Update v2

--- Jarvis.py
+++ Jarvis.py
@@ -1,7 +1,6 @@
-# imports for all necessary libraries
 import tensorflow as tf
 from sklearn.cluster import KMeans
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import pipeline, GPT3LMHeadModel, GPT2Tokenizer
 from stable_baselines3 import DQN, PPO
 from neo4j import GraphDatabase
 import requests
@@ -17,6 +16,32 @@ import threading
 import wikipedia
 import wolframalpha
 from homeassistant import HomeAssistant
+from homeassistant.const import (
+    STATE_ON,
+    STATE_OFF,
+)
+import socket
+import ssl
+import logging
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] - %(message)s')
+
+# SSL Context for secure communication with devices
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.verify_mode = ssl.CERT_REQUIRED
+context.check_hostname = True
+context.load_verify_locations("path_to_certificate")
+
+# List of all smart devices to control with their APIs
+devices = {
+    "living_room_light": {"api": "your_light_api", "state": STATE_OFF},
+    # add all your other devices similarly
+}
 
 # 1. Neural Network Model
 model = tf.keras.Sequential([
@@ -36,17 +61,17 @@ agent_advanced = PPO('MlpPolicy', env, verbose=1)
 agent_advanced.learn(total_timesteps=10000)
 
 # 3. Web Browser Integration
-# Properly replace the "your URL" with your target URL
-url = "your URL"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+def fetch_web_content(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup
 
 # 4. Unsupervised Learning
 kmeans = KMeans(n_clusters=3)
-# Assuming you have a dataset X
-# X = your_data
-# Uncomment the line below after setting your_data
-# kmeans.fit(X)
+# Load or create your dataset here
+X = None  # replace this with your actual data
+if X is not None:
+    kmeans.fit(X)
 
 # 5. Knowledge Base
 driver = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "password"))
@@ -54,38 +79,34 @@ with driver.session() as session:
     result = session.run("MATCH (n) RETURN n")
 
 # 6. Chatbot Functions
-# Previously, there was a mention of a "conversational" pipeline which does not exist.
-# We will replace this with "text-generation" pipeline
-from transformers import TextGenerationPipeline, GPT2LMHeadModel, GPT2Tokenizer
+chatbot = pipeline("conversational")
+
+# NLP setup for better communication abilities
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model_nlp = GPT2LMHeadModel.from_pretrained('gpt2')
-chatbot = TextGenerationPipeline(model=model_nlp, tokenizer=tokenizer)
+model_nlp = GPT3LMHeadModel.from_pretrained('gpt3')
 
 # Multi-modal abilities: Speech Recognition
 recognizer = sr.Recognizer()
 
 # Multi-modal abilities: Computer Vision
-# Replace 'path_to_prototxt_file', 'path_to_model_file' with your actual file paths
 model_cv = cv2.dnn.readNetFromCaffe('path_to_prototxt_file', 'path_to_model_file')
 
 # Predictive Model
 model_predictive = svm.SVC(gamma='scale')
 
 # Emotion AI Model
-# Replace 'path_to_emotion_model' with your actual model path
 model_emotion = load_model('path_to_emotion_model')
 
 # IoT device management
-# Replace "http://your-home-assistant-url" with your actual Home Assistant URL
 hass = HomeAssistant("http://your-home-assistant-url")
 
 # Personal assistant tasks and scheduling
 personal_calendar = {}  # A dictionary to store user's schedules
 
 # Wolfram Alpha for accessing factual information
-# Replace 'your-wolfram-alpha-app-id' with your actual Wolfram Alpha App ID
 wolframalpha_app_id = 'your-wolfram-alpha-app-id'
 wolframalpha_client = wolframalpha.Client(wolframalpha_app_id)
+
 # Multitasking via Python threading
 class JarvisTask(threading.Thread):
     def __init__(self, *args, **kwargs):
@@ -94,23 +115,20 @@ class JarvisTask(threading.Thread):
         self.start()
 
 def ask_jarvis(question):
-    response = chatbot(question)[0]['generated_text']
-    return response
+    try:
+        response = chatbot(question)[0]['generated_text']
+        return response
+    except Exception as e:
+        logging.error(f"Error in ask_jarvis: {str(e)}")
+        return "Sorry, I couldn't process that."
 
 # 7. Integration of all components
 class JarvisAI:
-    def __init__(self, model, agent, agent_advanced, driver, chatbot, model_cv, model_predictive, model_emotion, model_nlp, tokenizer, recognizer):
+    def __init__(self, model, agent, driver, chatbot):
         self.model = model
         self.agent = agent
-        self.agent_advanced = agent_advanced
         self.driver = driver
         self.chatbot = chatbot
-        self.model_cv = model_cv
-        self.model_predictive = model_predictive
-        self.model_emotion = model_emotion
-        self.model_nlp = model_nlp
-        self.tokenizer = tokenizer
-        self.recognizer = recognizer
 
     def predict(self, data):
         return self.model.predict(data)
@@ -119,9 +137,7 @@ class JarvisAI:
         return self.agent.predict(observation)
 
     def fetch_web_content(self, url):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        return soup
+        return fetch_web_content(url)
 
     def query_knowledge_base(self, query):
         with self.driver.session() as session:
@@ -129,36 +145,39 @@ class JarvisAI:
             return result
 
     def ask_jarvis(self, question):
-        response = self.chatbot(question)[0]['generated_text']
-        return response
+        return ask_jarvis(question)
 
     def listen(self):
         with sr.Microphone() as source:
-            audio = self.recognizer.listen(source)
-            # Here, the data format might need to be converted according to the requirement of your emotion model
-            return self.understand_emotion(audio)
+            audio = recognizer.listen(source)
+            return recognizer.recognize_google(audio)
 
     def see(self, image):
         blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
-        self.model_cv.setInput(blob)
-        detections = self.model_cv.forward()
+        model_cv.setInput(blob)
+        detections = model_cv.forward()
         return detections
 
     def understand_emotion(self, audio):
-        # Here you'll need to convert audio data to the format your emotion model expects,
-        # and then pass it through the model. This is just a placeholder:
-        emotion_prediction = self.model_emotion.predict(audio)
+        emotion_prediction = model_emotion.predict(audio)
         return emotion_prediction
 
     def make_advanced_decision(self, observation):
-        return self.agent_advanced.predict(observation)
+        return agent_advanced.predict(observation)
 
     def make_predictive_decision(self, data):
-        return self.model_predictive.predict(data)
+        return model_predictive.predict(data)
 
     def generate_text(self, prompt):
-        inputs = self.tokenizer.encode(prompt, return_tensors='pt')
-        outputs = self.model_nlp.generate(inputs, max_length=500, num_return_sequences=1)
-        # Now let's decode it and return the result
-        generated_text = self.tokenizer.decode(outputs[0])
-        return generated_text
+        inputs = tokenizer.encode(prompt, return_tensors='pt')
+        outputs = model_nlp.generate(inputs, max_length=500, num_return_sequences=1)
+
+    def control_iot_devices(self, command):
+        hass.send_command(command)
+
+    def schedule_tasks(self, task):
+        personal_calendar[datetime.now()] = task
+
+    def fetch_fact(self, query):
+        res = wolframalpha_client.query(query)
+        return next(res.results).text
